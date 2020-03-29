73d267b2f9705b986c4715328a18ed7a37e72828
Jonathan Cliby <jon.cliby@gmail.com>
2020-03-29 14:00:40 -0500

    Add post styling

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import parse from 'html-react-parser';
 
 import './Post.css';
+import PostImage from '../images/postImage.svg';
 
-const Post = () => {
+const Post = ({ post }) => {
+  const formatDate = () => {
+    return new Date(post.date).toDateString();
+  };
+
   return (
     <div id="post-container" className="container">
-      <div id="post-head">Blog</div>
-      Hello
+      <h1 className="post-title">{post.title}</h1>
+      <p className="post-date">{formatDate()}</p>
+      <img
+        src={PostImage}
+        id="post-image"
+        alt="Little doodle that has nothing to with post content"
+      />
+      <div className="post-content">{parse(post.content)}</div>
     </div>
   );
 };
